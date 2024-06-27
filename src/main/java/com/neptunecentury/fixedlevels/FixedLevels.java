0eaa041bb748d0ae2f532c61e57effa4c4885957
Eric Butler <e.c.butler@outlook.com>
2024-06-27 14:46:30 -0400

    Removed unused code

--- src/main/java/com/neptunecentury/fixedlevels/FixedLevels.java
+++ src/main/java/com/neptunecentury/fixedlevels/FixedLevels.java
@@ -3,14 +3,9 @@ package com.neptunecentury.fixedlevels;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FixedLevels implements ModInitializer {
 	public static final String MOD_ID = "fixed-levels";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	public static boolean curveMode = false;
 
 	@Override
 	public void onInitialize() {
